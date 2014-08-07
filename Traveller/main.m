1fdc904642224993e810d48307a11fff41184240
Hendra Uzia <hendra.uzia@gmail.com>
2014-08-07 10:43:56 +0700

    Initial Commit

---
+++ Traveller/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Traveller
+//
+//  Created by Video One on 8/7/14.
+//  Copyright (c) 2014 Hendra Uzia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
