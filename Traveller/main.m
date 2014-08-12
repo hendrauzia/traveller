b059fab17fee1c766c17f2a55f724e2ef01a9850
Hendra Uzia <hendra.uzia@gmail.com>
2014-08-12 13:14:34 +0700

    Refactor budget reduction and charging to functions

--- Traveller/main.m
+++ Traveller/main.m
@@ -7,24 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
-struct budget {
+
+typedef struct {
     float exchangeRate;
     double budget;
     double euroTransaction;
-};
+} budget;
 
-int main(int argc, const char * argv[])
-{
-    struct budget vacation;
+budget vacation;
+
+void spendDollars(double dollars) {
+    vacation.budget -= dollars;
+}
+
+void chargeEuros(double euros) {
+    vacation.euroTransaction = euros * vacation.exchangeRate;
+    vacation.budget -= vacation.euroTransaction;
+}
+
+int main(int argc, const char * argv[]) {
     vacation.exchangeRate = 1.2500;
     vacation.budget = 1000.00;
+    double numberDollars = 100;
+    double numberEuros = 100;
     
-    vacation.budget -= 100;
-    NSLog(@"Converting 100 US dollars into euros leaves $%.2f", vacation.budget);
+    spendDollars(numberDollars);
+    NSLog(@"Converting $%.2f US dollars into euros leaves $%.2f", numberDollars, vacation.budget);
     
-    vacation.euroTransaction = 100 * vacation.exchangeRate;
-    vacation.budget -= vacation.euroTransaction;
-    NSLog(@"Charging 100 euros leaves $%.2f", vacation.budget);
-
+    chargeEuros(numberEuros);
+    NSLog(@"Charging €%.2f euros leaves $%.2f", numberEuros, vacation.budget);
+    
     return 0;
 }
