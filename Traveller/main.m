6b78ee9e432ef728454badc464d57b46b60167b8
Hendra Uzia <hendra.uzia@gmail.com>
2014-08-17 08:29:02 +0700

    Refactor budget assignment to use getter and setter methods

--- Traveller/main.m
+++ Traveller/main.m
@@ -9,12 +9,13 @@
 #import <Foundation/Foundation.h>
 
 @interface Budget : NSObject {
-@public
-    float rate;
     double budget;
+    float rate;
     double transaction;
 }
 
+- (void) setBudget: (double) budgetValue withRate: (float) rateValue;
+- (double) getBudget;
 - (void) spend: (double) dollars;
 - (void) charge: (double) currency;
 @end
@@ -23,32 +24,37 @@ int main(int argc, const char * argv[]) {
     Budget *europe = [Budget new];
     Budget *england = [Budget new];
 
-    europe->rate = 1.2500;
-    europe->budget = 1000.00;
+    [europe setBudget:1000.00 withRate:1.2500];
     double dollarsInEurope = 100;
     double euros = 100;
-    
-    england->rate = 1.5000;
-    england->budget = 2000.00;
+
+    [england setBudget:2000.00 withRate:1.5000];
     double dollarsInEngland = 100;
     double pounds = 100;
     
     [europe spend:dollarsInEurope];
-    NSLog(@"Converting $%.2f US dollars into euros leaves $%.2f", dollarsInEurope, europe->budget);
+    NSLog(@"Converting $%.2f US dollars into euros leaves $%.2f", dollarsInEurope, [europe getBudget]);
     
     [europe charge:euros];
-    NSLog(@"Charging €%.2f euros leaves $%.2f", euros, europe->budget);
+    NSLog(@"Charging €%.2f euros leaves $%.2f", euros, [europe getBudget]);
     
     [england spend:dollarsInEngland];
-    NSLog(@"Converting $%.2f US dollars into pounds leaves $%.2f", dollarsInEngland, england->budget);
+    NSLog(@"Converting $%.2f US dollars into pounds leaves $%.2f", dollarsInEngland, [england getBudget]);
     
     [england charge:pounds];
-    NSLog(@"Charging €%.2f pounds leaves $%.2f", pounds, england->budget);
+    NSLog(@"Charging €%.2f pounds leaves $%.2f", pounds, [england getBudget]);
     
     return 0;
 }
 
 @implementation Budget
+- (void) setBudget: (double) budgetValue withRate: (float) rateValue {
+    budget = budgetValue;
+    rate = rateValue;
+}
+- (double) getBudget {
+    return budget;
+}
 - (void) spend: (double) dollars {
     budget -= dollars;
 }
