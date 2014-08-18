a420f9a377d30db6f57be1f450502ed8cce40ba7
Hendra Uzia <hendra.uzia@gmail.com>
2014-08-18 23:04:57 +0700

    Add multiple transaction for a single budget using NSMutableArray

--- Traveller/main.m
+++ Traveller/main.m
@@ -10,20 +10,26 @@
 #import "Budget.h"
 
 int main(int argc, const char * argv[]) {
-    Budget *europe = [Budget new];
-    Budget *england = [Budget new];
-
-    [europe setBudget:@1000.00 withRate:@1.2500];
-    NSNumber *dollarsInEurope = @100;
-    NSNumber *euros = @100;
-    
-    [england setBudget:@2000.00 withRate:@1.5000];
-    NSNumber *dollarsInEngland = @100;
     NSNumber *pounds = @100;
-    
-    [europe spend:dollarsInEurope];
-    [europe charge:euros];
+    NSNumber *euros = @100;
+    NSNumber *dollarsInEngland = @100;
 
+    NSNumber *dollarsInEurope = @100;
+    NSNumber *anotherDollarsInEurope = @200;
+    NSMutableArray *europeTransactions = [[NSMutableArray alloc] initWithCapacity:1 ];
+    
+    [europeTransactions addObject:dollarsInEurope];
+    [europeTransactions addObject:anotherDollarsInEurope];
+    
+    Budget *europe = [Budget new];
+    [europe setBudget:@1000.00 withRate:@1.2500];
+    [europe charge:euros];
+    for (NSNumber *transaction in europeTransactions) {
+        [europe spend:transaction];
+    }
+
+    Budget *england = [Budget new];
+    [england setBudget:@2000.00 withRate:@1.5000];
     [england spend:dollarsInEngland];
     [england charge:pounds];
     
